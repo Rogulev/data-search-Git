edd2fd4aebaf138c9aaf3b51e21e08330c4eb07b
Rogulev <Rogulev92@rambler.ru>
2020-07-02 19:14:19 +0500

    Добавил в таблицу аватары пользователей и стили на hover табличных строк

--- source/js/main.js
+++ source/js/main.js
@@ -3,12 +3,20 @@ const buttonSearch = document.querySelector('.button-search');
 const templateTr = document.querySelector('#template-tr').content.querySelector('tr');
 const bodyTable = document.querySelector('#body-table');
 
-// let select
-let repoSort = document.querySelector('#repo-sort');
-let repoQuantity = document.querySelector('#repo-quantity');
+// const select
+const repoSort = document.querySelector('#repo-sort');
+const repoQuantity = document.querySelector('#repo-quantity');
+
+// Ф-ия отрисовки новой таблички
+const showTable = () => {
+  bodyTable.textContent = '';
+  const sectionTable = document.querySelector('.section-table');
+  sectionTable.style.display = 'block';
+}
+
 
 // Запрос на сервер
-let getSearchResults = (url) => {
+const getSearchResults = (url) => {
   const xhr = new XMLHttpRequest();
   xhr.open('GET', url);
   xhr.send();
@@ -20,36 +28,31 @@ let getSearchResults = (url) => {
   xhr.addEventListener('load', () => {
     const result = JSON.parse(xhr.responseText).items;
     const fragment = document.createDocumentFragment();
+    showTable();
 
     result.forEach(data => {
-      let elementTr = templateTr.cloneNode(true)
-      let numRepo = elementTr.querySelector('.colNumber');
-      let repoTable = elementTr.querySelector('.colRepo');
-      let userTable = elementTr.querySelector('.colUser');
+      const elementTr = templateTr.cloneNode(true)
+      const numRepo = elementTr.querySelector('.colNumber');
+      const repoTable = elementTr.querySelector('.colRepo');
+      const userTable = elementTr.querySelector('.colUser');
+      const avatar = elementTr.querySelector('.avatar');
 
       numRepo.textContent = data.stargazers_count;
       repoTable.textContent = data.name;
       userTable.textContent = data.owner.login;
+      avatar.innerHTML = `<img src=https://avatars3.githubusercontent.com/u/${data.owner.id}?v=4>`;
 
-      fragment.appendChild(elementTr)
+      fragment.append(elementTr)
     });
 
-    document.querySelector('#body-table').appendChild(fragment)
+    document.querySelector('#body-table').append(fragment)
   })
 }
 
-// Ф-ия отрисовки новой таблички
-const showTable = () => {
-  bodyTable.textContent = '';
-  const sectionTable = document.querySelector('.section-table');
-  sectionTable.style.display = 'block';
-}
 
 // Событие отправки формы
-let searchForm = document.querySelector('#search-form');
+const searchForm = document.querySelector('#search-form');
 searchForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
-  showTable();
   getSearchResults(`https://api.github.com/search/repositories?q=${inputSearch.value}&per_page=${repoQuantity.value}&sort=${repoSort.value}`)
-
 })

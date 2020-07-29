ec5b645591a05ae9eb8df8c86d16a4a1bae2684d
Rogulev <Rogulev92@rambler.ru>
2020-07-29 16:11:23 +0500

    Создал вызов модального окна по клику с описанием и ссылками на гит // добавил стили на модальное окно

--- source/js/main.js
+++ source/js/main.js
@@ -14,9 +14,8 @@ const showTable = () => {
   sectionTable.style.display = 'block';
 }
 
-
 // Запрос на сервер
-const getSearchResults = (url) => {
+function getSearchResults(url) {
   const xhr = new XMLHttpRequest();
   xhr.open('GET', url);
   xhr.send();
@@ -26,33 +25,55 @@ const getSearchResults = (url) => {
   })
 
   xhr.addEventListener('load', () => {
-    const result = JSON.parse(xhr.responseText).items;
+    const results = JSON.parse(xhr.responseText).items;
     const fragment = document.createDocumentFragment();
     showTable();
 
-    result.forEach(data => {
-      const elementTr = templateTr.cloneNode(true)
-      const numRepo = elementTr.querySelector('.colNumber');
-      const repoTable = elementTr.querySelector('.colRepo');
-      const userTable = elementTr.querySelector('.colUser');
-      const avatar = elementTr.querySelector('.avatar');
+    results.forEach(data => {
+      const row = templateTr.cloneNode(true);
+      const numRepo = row.querySelector('.colNumber');
+      const repoTable = row.querySelector('.colRepo');
+      const userTable = row.querySelector('.colUser');
+      const avatar = row.querySelector('.colAvatar');
 
-      numRepo.textContent = data.stargazers_count;
-      repoTable.textContent = data.name;
-      userTable.textContent = data.owner.login;
-      avatar.innerHTML = `<img src=https://avatars3.githubusercontent.com/u/${data.owner.id}?v=4>`;
+      const stars = data.stargazers_count;
+      const repoName = data.name;
+      const userName = data.owner.login;
+      const userId = data.owner.id;
+      const userURL = data.owner.html_url;
+      const repoURL = data.html_url;
+      const forks = data.forks;
+      const lang = data.language;
 
-      fragment.append(elementTr)
+      numRepo.textContent = stars;
+      repoTable.textContent = repoName;
+      userTable.textContent = userName;
+      avatar.innerHTML = `<img src=https://avatars3.githubusercontent.com/u/${userId}?v=4>`;
+
+      row.addEventListener('click', () =>
+        openModal(userName, repoName, userId, repoURL, forks, lang, userURL));
+
+      fragment.append(row)
     });
 
     document.querySelector('#body-table').append(fragment)
   })
 }
 
-
 // Событие отправки формы
 const searchForm = document.querySelector('#search-form');
 searchForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
-  getSearchResults(`https://api.github.com/search/repositories?q=${inputSearch.value}&per_page=${repoQuantity.value}&sort=${repoSort.value}`)
-})
+  getSearchResults(`https://api.github.com/search/repositories?q=${inputSearch.value}&per_page=${repoQuantity.value}&sort=${repoSort.value}`);
+});
+
+const openModal = (userName, repoName, userId, repoURL, forks, lang, userURL) => {
+  $('#exampleModal').modal('show');
+  $('#exampleModalLabel a').text(`User name: ${userName}`)
+    .attr('href', `${userURL}`);
+  $('.main-repo a').text(`Repo name: ${repoName}`)
+    .attr('href', `${repoURL}`);
+  $('.user-avatar').html(`<img src=https://avatars3.githubusercontent.com/u/${userId}?v=4>`);
+  $('.info-list').empty().append(`<li>Forks: ${forks}</li>`)
+    .append(`<li>Language: ${lang}</li>`);
+};

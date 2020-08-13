adff7d5e88b898be884901ebde5f707d6d1204fa
Rogulev <Rogulev92@rambler.ru>
2020-08-13 22:08:53 +0500

    Переенес проект на jquery

--- source/js/main.js
+++ source/js/main.js
@@ -1,101 +1,95 @@
-const inputSearch = document.querySelector('.input-search');
-const buttonSearch = document.querySelector('.button-search');
-const templateTr = document.querySelector('#template-tr').content.querySelector('tr');
-const bodyTable = document.querySelector('#body-table');
+$(document).ready(function () {
 
-// const select
-const repoSort = document.querySelector('#repo-sort');
-const repoQuantity = document.querySelector('#repo-quantity');
+  const $inputSearch = $('.input-search');
+  // const select
+  const $repoSort = $('#repo-sort');
+  const $repoQuantity = $('#repo-quantity');
 
-// Ф-ия отрисовки новой таблички
-const showTable = () => {
-  bodyTable.textContent = '';
-  const sectionTable = document.querySelector('.section-table');
-  sectionTable.style.display = 'block';
-}
+  // Ф-ия отрисовки новой таблички
+  const $showTable = () => {
+    $('#body-table').text('');
+    $('.section-table').show()
+  }
 
-// Запрос на сервер
-function getSearchResults(url) {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.send();
+  // Событие отправки формы
+  $('#search-form').on('submit', (evt) => {
+    evt.preventDefault();
+    getSearchResults();
+  });
 
-  xhr.addEventListener('error', () => {
-    alert('Ошибка поиска, повторите попытку')
-  })
+  const openModal = ({
+    repoName,
+    stars,
+    userName,
+    userId,
+    userURL,
+    repoURL,
+    forks,
+    lang
+  }) => {
+    $('#exampleModal').modal('show');
+    $('#exampleModalLabel a  span').text(repoName);
+    $('#exampleModalLabel a').attr('href', userURL);
+    $('.main-repo a  span').text(userName);
+    $('.main-repo a').attr('href', repoURL);
+    $('.user-avatar').html(`<img src=https://avatars3.githubusercontent.com/u/${userId}?v=4>`);
+    $('.details__icon--forks span').text(forks);
+    $('.details__icon--stars span').text(stars);
+    $('.details__icon--language svg').attr('fill', langColors[lang]);
+    $('.details__icon--language span').text(lang);
+  };
 
-  xhr.addEventListener('load', () => {
-    const results = JSON.parse(xhr.responseText).items;
-    const fragment = document.createDocumentFragment();
 
-    if (results.length <= 0) {
-      $('.section-table').hide();
-      $('.error-section').show();
-    } else {
-      $('.error-section').hide();
-      showTable();
+  // Запрос на сервер
+  function getSearchResults() {
+    $.ajax({
+      type: 'GET',
+      url: `https://api.github.com/search/repositories?q=${$inputSearch.val()}&per_page=${$repoQuantity.val()}&sort=${$repoSort.val()}`,
+      success: function (data) {
+        const $results = $(data.items);
+        const $fragment = $(document.createDocumentFragment());
+        if ($results.length <= 0) {
+          $('.section-table').hide();
+          $('.error-section').show();
+        } else {
+          $('.error-section').hide();
+          $showTable();
 
-      results.forEach(data => {
-        const row = templateTr.cloneNode(true);
-        const numRepo = row.querySelector('.colNumber');
-        const repoTable = row.querySelector('.colRepo');
-        const userTable = row.querySelector('.colUser');
-        const avatar = row.querySelector('.colAvatar');
+          $results.each((index, value) => {
+            const $templateTr = $('#template-tr').clone('tr').contents();
+            const $numRepo = $($templateTr).find('.colNumber');
+            const $repoTable = $($templateTr).find('.colRepo');
+            const $userTable = $($templateTr).find('.colUser');
+            const $avatar = $($templateTr).find('.colAvatar');
 
-        const repoInfo = {
-          repoName: data.name,
-          stars: data.stargazers_count,
-          userName: data.owner.login,
-          userId: data.owner.id,
-          userURL: data.owner.html_url,
-          repoURL: data.html_url,
-          forks: data.forks,
-          lang: data.language
-        }
+            const repoInfo = {
+              repoName: value.name,
+              stars: value.stargazers_count,
+              userName: value.owner.login,
+              userId: value.owner.id,
+              userURL: value.owner.html_url,
+              repoURL: value.html_url,
+              forks: value.forks,
+              lang: value.language
+            }
 
-        numRepo.textContent = repoInfo.stars;
-        repoTable.textContent = repoInfo.repoName;
-        userTable.textContent = repoInfo.userName;
-        avatar.innerHTML = `<img src=https://avatars3.githubusercontent.com/u/${repoInfo.userId}?v=4>`;
+            $numRepo.text(repoInfo.stars);
+            $repoTable.text(repoInfo.repoName);
+            $userTable.text(repoInfo.userName);
+            $avatar.html(`<img src=https://avatars3.githubusercontent.com/u/${repoInfo.userId}?v=4>`);
 
-        row.addEventListener('click', () =>
-          openModal(repoInfo));
+            $templateTr.on('click', () =>
+              openModal(repoInfo));
 
-        fragment.append(row)
-      });
+            $fragment.append($templateTr);
+            console.log($fragment);
+          });
+          console.log($results)
+          $('#body-table').append($fragment)
+          console.log($fragment)
+        };
 
-      document.querySelector('#body-table').append(fragment)
-    };
-  })
-}
-
-// Событие отправки формы
-const searchForm = document.querySelector('#search-form');
-searchForm.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  getSearchResults(`https://api.github.com/search/repositories?q=${inputSearch.value}&per_page=${repoQuantity.value}&sort=${repoSort.value}`);
+      }
+    })
+  };
 });
-
-const openModal = ({
-  repoName,
-  stars,
-  userName,
-  userId,
-  userURL,
-  repoURL,
-  forks,
-  lang
-}) => {
-  $('#exampleModal').modal('show');
-  $('#exampleModalLabel a  span').text(repoName);
-  $('#exampleModalLabel a').attr('href', userURL);
-  $('.main-repo a  span').text(userName);
-  $('.main-repo a').attr('href', repoURL);
-  $('.user-avatar').html(`<img src=https://avatars3.githubusercontent.com/u/${userId}?v=4>`);
-  $('.details__icon--forks span').text(forks);
-  $('.details__icon--stars span').text(stars);
-  $('.details__icon--language svg').attr('fill', langColors[lang]);
-  $('.details__icon--language span').text(lang);
-};
-
-
